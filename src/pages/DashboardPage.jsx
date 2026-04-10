e5e52047130344e7a439cefe17265df279a9d94d
TemplatesCrate <official.aryan427@gmail.com>
2026-04-10 14:57:01 +0530

    Update DashboardPage.jsx

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,8 +1,13 @@
-// DashboardPage.jsx
 import React from 'react';
+import styles from './DashboardPage.module.css';
 
 const DashboardPage = () => {
-    return <div>Dashboard Page</div>;
+  return (
+    <div className={styles.container}>
+      <h1>Dashboard</h1>
+      <p>Coming soon...</p>
+    </div>
+  );
 };
 
 export default DashboardPage;
